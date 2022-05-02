661cb5e71bf3c4683d82a17930b010769cec7b9a
Your Name <you@example.com>
2022-05-02 18:21:33 +0200

    env

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MoralisProvider } from "react-moralis";
+// import "dotenv/config";
 
 ReactDOM.render(
   <React.StrictMode>

56433e41c190f4b727dfbd9f0dee6ab90a6372e3
Robertas <->
2022-05-08 15:06:14 +0300

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,20 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  BrowserRouter,
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginButton from "./components/LoginButton";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
         <LoginButton />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 

419a88e363317eb4ce084bf9e3b4188424893aab
Robertas <->
2022-05-08 16:56:14 +0300

    AuthButton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginButton from "./components/LoginButton";
+import Account from "./components/Acount/Account";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <LoginButton />
+        <Account />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />

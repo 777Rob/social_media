cc5917869dd62c75d1bddaf5bee0a0fb9694ace5
Robertas <->
2022-05-08 14:49:00 +0300

    theme+mantineprovider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,15 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MoralisProvider } from "react-moralis";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider
-      serverUrl={process.env.REACT_APP_SERVER_URL || ""}
-      appId={process.env.REACT_APP_MORALIS_APP_ID || ""}
-    >
-      <App />
-    </MoralisProvider>
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <MoralisProvider
+        serverUrl={process.env.REACT_APP_SERVER_URL}
+        appId={process.env.REACT_APP_MORALIS_APP_ID}
+      >
+        <App />
+      </MoralisProvider>
+    </MantineProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ client/src/theme.tsx
@@ -0,0 +1,8 @@
+import { MantineThemeOverride } from "@mantine/core";
+
+export const theme: MantineThemeOverride = {
+  colorScheme: "light",
+  primaryColor: "orange",
+  defaultRadius: 0,
+  fontFamily: "Open Sans",
+};

59e0a6a350d5a9a3891f764c6a096a8fcef71aed
Robertas <->
2022-05-08 15:03:21 +0300

    LoginButton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,15 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useMoralis } from "react-moralis";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginButton from "./components/LoginButton";
 
-function App() {
-  const {
-    authenticate,
-    isAuthenticated,
-    isAuthenticating,
-    user,
-    account,
-    logout,
-  } = useMoralis();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      // add your logic here
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated]);
-
-  const login = async () => {
-    if (!isAuthenticated) {
-      await authenticate({ signingMessage: "Log in using Moralis" })
-        .then(function (user) {
-          console.log("logged in user:", user);
-          console.log(user!.get("ethAddress"));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-  };
-
-  const logOut = async () => {
-    await logout();
-    console.log("logged out");
-  };
-
+const App = () => {
   return (
     <div>
-      <h1>Moralis Hello World!</h1>
-      <button onClick={login}>Moralis Metamask Login</button>
-      <button onClick={logOut} disabled={isAuthenticating}>
-        Logout
-      </button>
+      <LoginButton />
     </div>
   );
-}
+};
 
 export default App;
--- client/src/theme.tsx
+++ client/src/theme.tsx
@@ -1,8 +1,8 @@
 import { MantineThemeOverride } from "@mantine/core";
 
 export const theme: MantineThemeOverride = {
-  colorScheme: "light",
+  colorScheme: "dark",
   primaryColor: "orange",
   defaultRadius: 0,
-  fontFamily: "Open Sans",
+  fontFamily: "Greycliff CF, sans-serif",
 };

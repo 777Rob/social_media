56eadfe5b32d475061a9d99a2402cb722c98a7b1
Vakaris Paulavičius <76004884+pvakaris@users.noreply.github.com>
2022-05-14 19:19:18 +0300

    Delete client directory

--- client/src/TestText.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button } from '@mantine/core';
-
-export default function TestText() {
-    return <Button radius="xl" compact uppercase variant="gradient" gradient={{ from: '#ed6ea0', to: '#ec8c69', deg: 35 }}>
-    Settings
-  </Button>;
-
-}
--- client/src/theme.tsx
+++
@@ -1,8 +0,0 @@
-import { MantineThemeOverride } from "@mantine/core";
-
-export const theme: MantineThemeOverride = {
-  colorScheme: "light",
-  primaryColor: "orange",
-  defaultRadius: 0,
-  fontFamily: "Greycliff CF, sans-serif",
-};
